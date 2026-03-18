d4030468436718d5bee8101fbedf25d7a60a5108
sepciclovro <sepciclovro@gmail.com>
2026-03-18 18:40:08 +0100

    Update vjezba1.php

--- vjezba1.php
+++ vjezba1.php
@@ -3,7 +3,7 @@ $naslov = "Moj prvi PHP dokument";
 $autor = "Lovro Sepčić";
 
 echo "<h1>$naslov</h1>";
-echo "<p>Ovu stranicu izradio/la je <strong>$autor</strong>.</p>";
+echo "<p>Ovu stranicu izradio/la je <strong>Lovro Sepčić</strong>.</p>";
 echo '<a href="https://www.sczg.unizg.hr/" target="_blank">Posjeti stranicu Studentskog centra u Zagrebu</a>';
 ?>
 <!-- Naziv datoteke: vjezba1.php -->
