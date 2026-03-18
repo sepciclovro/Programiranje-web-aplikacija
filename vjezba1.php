312aa5e854e0ca8fef4b1bc0a21d48ceb2515109
sepciclovro <sepciclovro@gmail.com>
2026-03-18 18:30:58 +0100

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,9 @@
+<?php
+$naslov = "Moj prvi PHP dokument";
+$autor = "Lovro Sepčić";
+
+echo "<h1>$naslov</h1>";
+echo "<p>Ovu stranicu izradio/la je <strong>$autor</strong>.</p>";
+echo '<a href="https://www.sczg.unizg.hr/" target="_blank">Posjeti stranicu Studentskog centra u Zagrebu</a>';
+?>
+<!-- Naziv datoteke: vjezba1.php -->
